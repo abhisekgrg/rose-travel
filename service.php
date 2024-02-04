66a285f8d8cd36fd94d90e250afa2aeb09d4139c
Rana-tech <abhisek.gurung590@gmail.com>
2024-02-04 23:11:22 +0545

    blog, service header updated

--- service.php
+++ service.php
@@ -1,1 +1,89 @@
-<h1>service</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php 
+    include "header.php";
+ 
+    ?>
+    <div class="section-banner flex ">
+
+        <div class="box container-banner flex align-item justify-content gradient-overlay">
+            <div class="banner-title flex flex-direction justify-content align-items row-gap-10">
+                <h2 class="banner-h2 flex justify-content">Services</h2>
+                <h3 class="banner-h3"> <a style="color:white;text-decoration:none;" href="index.php"> Home</a> /
+                    Services</h3>
+            </div>
+        </div>
+    </div>
+    <div class="section-space">
+        <div class="box grid-container">
+            <div class="flex column-gap-20" style="    border: 1px solid #DDE0E5;
+    padding: 20px;">
+                <div><img src="images/2.png" alt=""></div>
+                <div class="flex flex-direction row-gap-10">
+                    <h3 class="black">Best Guide</h3>
+                    <p>Plan your journey seamlessly with our user-friendly tools for <br>
+                        a personalized and hassle-free experience.</p>
+                </div>
+            </div>
+
+            <div class="flex column-gap-20" style="    border: 1px solid #DDE0E5;
+    padding: 20px;">
+                <div><img src="images/2.png" alt=""></div>
+                <div class="flex flex-direction row-gap-10">
+                    <h3 class="black">Best Guide</h3>
+                    <p>Plan your journey seamlessly with our user-friendly tools for <br>
+                        a personalized and hassle-free experience.</p>
+                </div>
+            </div>
+            <div class="flex column-gap-20" style="    border: 1px solid #DDE0E5;
+    padding: 20px;">
+                <div><img src="images/2.png" alt=""></div>
+                <div class="flex flex-direction row-gap-10">
+                    <h3 class="black">Best Guide</h3>
+                    <p>Plan your journey seamlessly with our user-friendly tools for <br>
+                        a personalized and hassle-free experience.</p>
+                </div>
+            </div>
+            <div class="flex column-gap-20" style="    border: 1px solid #DDE0E5;
+    padding: 20px;">
+                <div><img src="images/2.png" alt=""></div>
+                <div class="flex flex-direction row-gap-10">
+                    <h3 class="black">Best Guide</h3>
+                    <p>Plan your journey seamlessly with our user-friendly tools for <br>
+                        a personalized and hassle-free experience.</p>
+                </div>
+            </div>
+            <div class="flex column-gap-20" style="    border: 1px solid #DDE0E5;
+                 padding: 20px;">
+                <div><img src="images/2.png" alt=""></div>
+                <div class="flex flex-direction row-gap-10">
+                    <h3 class="black">Best Guide</h3>
+                    <p>Plan your journey seamlessly with our user-friendly tools for <br>
+                        a personalized and hassle-free experience.</p>
+                </div>
+            </div>
+            <div class="flex column-gap-20" style="    border: 1px solid #DDE0E5;
+                 padding: 20px;">
+                <div><img src="images/2.png" alt=""></div>
+                <div class="flex flex-direction row-gap-10">
+                    <h3 class="black">Best Guide</h3>
+                    <p>Plan your journey seamlessly with our user-friendly tools for <br>
+                        a personalized and hassle-free experience.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+include "footer.php";
+?>
+</body>
+
+</html>
